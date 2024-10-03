f677fcc828ccda391ded8b52a5951a3859f678da
Shreyam Pokharel <pshreyam@gmail.com>
2024-10-03 21:30:53 +0545

    Refactor the id for urls

--- app/models.py
+++ app/models.py
@@ -1,10 +1,15 @@
+import uuid
+
 from app import db
 
 
 class URL(db.Model):
     __tablename__ = "URL"
-    id = db.Column(db.Integer, primary_key=True)
+
+    id = db.Column(db.String(255), primary_key=True, default=lambda: str(uuid.uuid4()))
     full = db.Column(db.String(255), nullable=False)
-    short = db.Column(db.String(255), nullable=False, unique=True)
     clicks = db.Column(db.Integer, default=0)
     user = db.Column(db.String(255), nullable=False)
+
+    def __repr__(self):
+        return f"<URL | full={self.full} | id={self.id}>"
